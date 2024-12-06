dddcf3f2570f9dabd800489e889b073b2ff99b5c
Benjamín Montenegro <126958886+bmontenegrom@users.noreply.github.com>
2024-12-06 13:20:26 -0300

    se arregal dia 5

--- src/day5.rs
+++ src/day5.rs
@@ -1,33 +1,21 @@
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq, Eq)]
-struct Manual {
-    first: u32,
-    second: u32,
-}
 
-impl Manual {
-    fn new(first: u32, second: u32) -> Manual {
-        Manual { first, second }
-    }
-}
-
-fn parse_day5(input: &str) -> (Vec<Manual>, Vec<Vec<u32>>) {
+fn parse_day5(input: &str) -> (HashMap<u32, Vec<u32>>, Vec<Vec<u32>>) {
     let (rules, pages) = input
         .split_once("\n\n")
         .expect("deberia haber reglas y paginas");
-    let rules: Vec<Manual> = rules
+    let map =rules
         .lines()
-        .map(|l| {
+        .fold(HashMap::new(), |mut acc, l| {
             let (first, second) = l
                 .split_once("|")
                 .expect("deberia haber numeros separados por |");
-            Manual::new(
-                first.parse().expect("deberia ser un numero"),
-                second.parse().expect("deberia se run numero"),
-            )
-        })
-        .collect();
+            acc.entry(first.parse::<u32>().expect("deberia ser un numero"))
+                .or_insert_with(Vec::new)
+                .push(second.parse::<u32>().expect("deberia ser un numero"));
+            acc
+        });
 
     let updates: Vec<Vec<u32>> = pages
         .lines()
@@ -39,55 +27,33 @@ fn parse_day5(input: &str) -> (Vec<Manual>, Vec<Vec<u32>>) {
             order
         })
         .collect();
-    (rules, updates)
+    (map, updates)
 }
 
-fn check_update(rules: &[Manual], update: &[u32]) -> bool {
-    let mut map = HashMap::new();
-    update.iter().enumerate().for_each(|(i, page)| {
-        map.insert(page, i);
-    });
-    rules.iter().all(|rule| {
-        if let Some(index_first) = map.get(&rule.first) {
-            if let Some(index_second) = map.get(&rule.second) {
-                index_first < index_second
+fn check_update(rules: &HashMap<u32, Vec<u32>>, update: &[u32]) -> bool {
+    update.iter().is_sorted_by(|&a, &b|{
+        rules.get(a).map_or(std::cmp::Ordering::Greater, |v| {
+            if v.contains(b) {
+                std::cmp::Ordering::Less
             } else {
-                true
+                std::cmp::Ordering::Greater
             }
-        } else {
-            true
-        }
+        }) == std::cmp::Ordering::Less
     })
 }
 
-fn correct_update(rules: &[Manual], update: &[u32]) -> Vec<u32> {
-    let mut map = HashMap::new();
-    update.iter().enumerate().for_each(|(i, page)| {
-        map.insert(page, i);
-    });
-    let mut correct = false;
-    while !correct {
-        correct = rules.iter().all(|rule| {
-            if let Some(&index_first) = map.get(&rule.first) {
-                if let Some(&index_second) = map.get(&rule.second) {
-                    if index_first >= index_second {
-                        map.entry(&rule.first).and_modify(|e| *e = index_second);
-                        map.entry(&rule.second).and_modify(|e| *e = index_first);
-                    }
-                    false
-                } else {
-                    true
-                }
+fn correct_update(rules: &HashMap<u32, Vec<u32>>, update: &[u32]) -> Vec<u32>{
+    let mut new_update = update.to_vec();
+    new_update.sort_by(|a, b|{
+        rules.get(a).map_or(std::cmp::Ordering::Greater, |v| {
+            if v.contains(b) {
+                std::cmp::Ordering::Less
             } else {
-                true
+                std::cmp::Ordering::Greater
             }
         })
-    }
-    let mut res: Vec<u32> = vec![0; update.len()];
-    for (key, value) in map {
-        res[value] = *key;
-    }
-    res
+    });
+    new_update
 }
 
 #[aoc(day5, part1)]
@@ -121,49 +87,6 @@ fn part2(input: &str) -> u32 {
 #[cfg(test)]
 mod test {
     use super::*;
-    #[test]
-    fn test_parser_gay5() {
-        let input = "47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47";
-        let (rules, pages) = parse_day5(input);
-        for rule in rules {
-            println!("firsts: {} second : {}", rule.first, rule.second);
-        }
-
-        for vec in pages {
-            for page in vec {
-                print!(" {}", page);
-            }
-            println!();
-        }
-    }
-
     #[test]
     fn test_day5_part1() {
         let input = "47|53
