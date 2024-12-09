5065587a1fee1a84ec6f918bee50dd8fb219c88d
Benjamín Montenegro <126958886+bmontenegrom@users.noreply.github.com>
2024-12-09 13:38:41 -0300

    dia 7 completo y corregido

--- src/day7.rs
+++ src/day7.rs
@@ -4,6 +4,7 @@ use std::collections::VecDeque;
 enum Operacion {
     Sum,
     Multiplication,
+    Concat,
 }
 
 #[derive(Debug)]
@@ -61,8 +62,9 @@ fn test_eq(ecuacion: &Ecuacion) -> bool {
             match operador.operacion {
                 Operacion::Sum => acc += termino,
                 Operacion::Multiplication => acc *= termino,
+                _=> panic!("operacion no soportada"),
             }
-            if acc < ecuacion.value {
+            if acc <= ecuacion.value && !terms.is_empty() {
                 new_stack.push(Operador::new(acc, Operacion::Sum));
                 new_stack.push(Operador::new(acc, Operacion::Multiplication));
             } else if acc == ecuacion.value && terms.is_empty() {
@@ -88,10 +90,98 @@ fn day7_part1(input: &str) -> u64 {
     })
 }
 
+
+fn test_eq_rec(value: u64, terms: &mut VecDeque<u64>) -> bool {
+    if terms.is_empty(){
+        return false;
+    } else if terms.len() == 1{
+        return terms[0] == value;
+    } else if terms[0] > value{
+        return false;
+    }
+    let left = terms.pop_front().expect("deberia haber al menos 2 valores");
+    let right = terms.pop_front().expect("deberia haber al menos 2 valores");
+    terms.push_front(left + right);
+    if test_eq_rec(value, terms){
+        return true;
+    }
+    terms.pop_front();
+    terms.push_front(left * right);
+    if test_eq_rec(value, terms){
+        return true;
+    }
+
+    terms.pop_front();
+    terms.push_front(right);
+    terms.push_front(left);
+    false
+}
+
+
+#[aoc(day7, part1, rec)]
+fn day7_part1_rec(input: &str) -> u64 {
+    let ecuaciones = parse_day7(input);
+    ecuaciones.iter().fold(0, |mut acc, e| {
+        if test_eq_rec(e.value, &mut e.terms.clone().into()){
+            acc += e.value;
+        }
+        acc
+    })
+
+}
+
+
+fn test_eq_part2(ecuacion: &Ecuacion)-> bool{
+    let mut terms: VecDeque<u64> = ecuacion.terms.clone().into();
+    let mut stack = vec![];
+    let mut termino = terms.pop_front().expect("deberia haber al menos 2 valores");
+    stack.push(Operador::new(termino, Operacion::Sum));
+    stack.push(Operador::new(termino, Operacion::Multiplication));
+    stack.push(Operador::new(termino, Operacion::Concat));
+    while !terms.is_empty() {
+        termino = terms
+            .pop_front()
+            .expect("deberia haber al menos 1 elemento");
+        let mut new_stack = Vec::new();
+        for operador in stack {
+            let mut acc = operador.termino;
+            match operador.operacion {
+                Operacion::Sum => acc += termino,
+                Operacion::Multiplication => acc *= termino,
+                Operacion::Concat=> acc = format!("{}{}", acc, termino).parse::<u64>().expect("deberia ser un numero"),
+            }
+            if acc <= ecuacion.value && !terms.is_empty() {
+                new_stack.push(Operador::new(acc, Operacion::Sum));
+                new_stack.push(Operador::new(acc, Operacion::Multiplication));
+                new_stack.push(Operador::new(acc, Operacion::Concat));
+            } else if acc == ecuacion.value && terms.is_empty() {
+                return true;
+            }
+        }
+        stack = new_stack;
+        if stack.is_empty() {
+            return false;
+        }
+    }
+    false
+}
+
+#[aoc(day7, part2)]
+fn day7_part2(input: &str) -> u64 {
+    let ecuaciones = parse_day7(input);
+    ecuaciones.iter().fold(0, |mut acc, e| {
+        if test_eq_part2(e) {
+            acc += e.value;
+        }
+        acc
+    })
+}
+
+
 mod test {
     use super::*;
     #[test]
-    fn test_parser() {
+    fn test_day7_part1() {
         let input = "190: 10 19
 3267: 81 40 27
 83: 17 5
@@ -102,5 +192,20 @@ mod test {
 21037: 9 7 18 13
 292: 11 6 16 20";
         assert_eq!(day7_part1(input), 3749);
+    }
+
+
+    #[test]
+    fn test_day7_part2(){
+        let input = "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
+        assert_eq!(day7_part2(input), 11387);
     }
 }
