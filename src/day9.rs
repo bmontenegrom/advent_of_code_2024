a0c8be09c7716f05150fdcc4ddbdc49b346e42a5
Benjamín Montenegro <126958886+bmontenegrom@users.noreply.github.com>
2024-12-18 11:35:53 -0300

    day 9 complete

--- src/day9.rs
+++ src/day9.rs
@@ -1,39 +1,27 @@
 #[derive(Debug, PartialEq, Eq)]
-enum FType{
+enum FType {
     File,
     Free,
 }
 
 #[derive(Debug, PartialEq, Eq)]
-struct Block{
+struct Block {
     ftype: FType,
-    id: usize,
-    used: usize,
     free: usize,
     data: Vec<usize>,
 }
 
 impl Block {
-    fn new(index: usize, size: usize) -> Block {
-        let ftype = if index % 2 == 0 {
-            FType::File
-        } else {
-            FType::Free
-        };
-        let id = index/2;
-        if ftype == FType::File {
+    fn new(id: usize, size: usize) -> Self {
+        if id % 2 == 0 {
             Block {
-                ftype,
-                id,
-                used: size,
+                ftype: FType::File,
                 free: 0,
-                data: vec![id; size],
+                data: vec![id / 2; size],
             }
         } else {
             Block {
-                ftype,
-                id,
-                used: 0,
+                ftype: FType::Free,
                 free: size,
                 data: Vec::new(),
             }
@@ -41,66 +29,33 @@ impl Block {
     }
 }
 
-
-
-
-
-
-fn parse_day9_part2(input: &str)-> Vec<Block>{
-    input.chars().map(|c| c.to_digit(10).expect("deberia ser digito")).enumerate().map(|(i, d)| {
-        Block::new(i, d as usize)
-    }).collect()
+fn parse_day9_part2(input: &str) -> Vec<Block> {
+    input
+        .chars()
+        .map(|c| c.to_digit(10).expect("deberia ser digiyo") as usize)
+        .enumerate()
+        .map(|(i, d)| Block::new(i, d))
+        .collect()
 }
 
-fn parse_day9_input(input: &str) ->Vec<Option<usize>>{
-    input.chars().map(|c| c.to_digit(10).expect("deberia ser digito")).enumerate().fold(Vec::new(), |mut acc, (i, d)| {
-        if i % 2 == 0{
-            for _ in 0..d{
-                acc.push(Some(i /2));
+fn parse_day9_input(input: &str) -> Vec<Option<usize>> {
+    input
+        .chars()
+        .map(|c| c.to_digit(10).expect("deberia ser digito"))
+        .enumerate()
+        .fold(Vec::new(), |mut acc, (i, d)| {
+            if i % 2 == 0 {
+                for _ in 0..d {
+                    acc.push(Some(i / 2));
+                }
+            } else {
+                for _ in 0..d {
+                    acc.push(None);
+                }
             }
-        }
-        else{
-            for _ in 0..d{
-                acc.push(None);
-            }
-        }
-        acc
-    })
+            acc
+        })
 }
-
-fn day9_part2(input: &str)->usize{
-    let mut disco = parse_day9_part2(input);
-    let mut right = disco.len() - 1;
-    while right > 0 {
-        while disco[right].ftype == FType::Free {
-            right -= 1;            
-        }
-        let mut left = 0_usize;
-        while left < right && (disco[left].ftype == FType::File || (disco[left].ftype == FType::Free  && disco[left].free < disco[right].used)) {
-            left += 1;
-        }
-        if left < right {
-            let right_data = &mut disco[right].data.clone();
-            disco[left].data.append(right_data);
-            disco[left].used += disco[right].used;
-            disco[left].free -= disco[right].used;
-            if disco[left].free == 0 {
-                disco[left].ftype = FType::File;
-            }
-            disco[right].ftype = FType::Free;
-            disco[right].used = 0;
-            disco[right].data.clear();
-        }
-    }
-    disco.iter().flat_map(|b|{
-        b.data.iter().cloned().chain(std::iter::repeat(0).take(b.free))
-    }).enumerate().fold(0, |mut acc, (i, x)|{
-        acc += x * i;
-        acc
-    })
-    
-}
-
 
 #[aoc(day9, part1)]
 fn day9_part1(input: &str) -> usize {
@@ -118,14 +73,57 @@ fn day9_part1(input: &str) -> usize {
             disco.swap(left, right);
         }
     }
-    disco.iter().enumerate().filter_map(|(i, d)| {
-        d.as_ref().map(|x| *x * i)
-    }).sum()
-
+    disco
+        .iter()
+        .enumerate()
+        .filter_map(|(i, d)| d.as_ref().map(|x| *x * i))
+        .sum()
 }
 
+#[aoc(day9, part2)]
+fn day9_part2(input: &str) -> usize {
+    let mut disco = parse_day9_part2(input);
+    let mut right = disco.len() - 1;
+    while right > 0 {
+        while disco[right].ftype == FType::Free {
+            right -= 1;
+        }
 
+        let mut left = 0;
 
+        while left < right {
+            if disco[left].ftype == FType::File
+                || (disco[left].ftype == FType::Free && disco[left].free < disco[right].data.len())
+            {
+                left += 1;
+                continue;
+            }
+
+            let mut aux = disco[right].data.clone();
+            disco[left].data.append(&mut aux);
+            disco[left].free -= disco[right].data.len();
+            if disco[left].free == 0 {
+                disco[left].ftype = FType::File;
+            }
+            disco[right].ftype = FType::Free;
+            disco[right].free = disco[right].data.len();
+            disco[right].data.clear();
+            break;
+        }
+        right -= 1;
+    }
+    disco
+        .iter()
+        .flat_map(|b| {
+            b.data
+                .iter()
+                .cloned()
+                .chain(std::iter::repeat(0).take(b.free))
+        })
+        .enumerate()
+        .map(|(i, d)| i * d)
+        .sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -138,8 +136,8 @@ mod tests {
     }
 
     #[test]
-    fn test_day9_part2(){
-        let input =  "2333133121414131402";
+    fn test_day9_part2() {
+        let input = "2333133121414131402";
         assert_eq!(day9_part2(input), 2858);
     }
 }
